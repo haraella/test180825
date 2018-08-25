8025b6435def5148d098869a6a520e49eba4290d
正志 <@HARARA>
2018-08-25 22:09:47 +0900

    あいいうえ

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(131, 114);
+            this.button1.Location = new System.Drawing.Point(38, 130);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
